5c90012f3cf661f549957294818ea21af2db2c13
Pawel Czajka <paw787878@gmail.com>
2023-05-03 16:39:24 +0200

    now can send date parameters

--- src/main/kotlin/com/example/ticketbookingapp/dataTransferObject/dataTransferObject.kt
+++ src/main/kotlin/com/example/ticketbookingapp/dataTransferObject/dataTransferObject.kt
@@ -1,8 +1,11 @@
 ﻿package com.example.ticketbookingapp.dataTransferObject
 
+import org.springframework.format.annotation.DateTimeFormat
 import java.util.Date
 
-data class ScreaningSelection(
+data class ScreeningSelectionDTO(
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     val minimalStartTime: Date,
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     val maximalEndTime: Date,
 )
---
+++ src/main/kotlin/com/example/ticketbookingapp/webApi/WebApi.kt
@@ -0,0 +1,21 @@
+﻿package com.example.ticketbookingapp.webApi
+
+import com.example.ticketbookingapp.dataTransferObject.ScreeningSelectionDTO
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(
+    path = ["/api"],
+    produces = ["application/json"]
+)
+// TODO_PAWEL should it be oher?
+@CrossOrigin(origins= ["*"])
+class WebApi {
+
+    @GetMapping("/screenings", consumes = ["application/json"])
+    fun getScreeningsInSelectedTime(dto: ScreeningSelectionDTO): Int {
+        // TODO_PAWEL
+        val a = 5;
+        return 5
+    }
+}
