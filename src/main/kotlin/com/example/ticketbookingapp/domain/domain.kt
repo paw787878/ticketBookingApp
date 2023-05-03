1f97d4b73eac00a5e6b12a98ba55b55055f567e0
Pawel Czajka <paw787878@gmail.com>
2023-05-03 12:16:52 +0200

    created domain entities

---
+++ src/main/kotlin/com/example/ticketbookingapp/domain/domain.kt
@@ -0,0 +1,62 @@
+﻿package com.example.ticketbookingapp.domain
+
+import jakarta.persistence.*
+import java.util.Date
+
+@Embeddable
+class User(
+    var name: String,
+    var surname: String,
+)
+
+@Entity
+class Movie(
+    val title: String,
+    @Id @GeneratedValue
+    val id: Long = 0,
+)
+
+@Entity
+class ScreeningRoom(
+    val name: String,
+    @Id @GeneratedValue
+    val id: Long = 0,
+)
+
+@Entity
+class MovieScreening(
+    @ManyToOne(fetch = FetchType.LAZY)
+    val movie: Movie,
+    @ManyToOne(fetch = FetchType.LAZY)
+    val screeningRoom: ScreeningRoom,
+    val timeOfStart: Date,
+    val timeOfEnd: Date,
+    @Id @GeneratedValue
+    val id: Long = 0,
+)
+
+@Entity
+class Reservation(
+    val user: User,
+    @ManyToOne(fetch = FetchType.LAZY)
+    val movieScreening: MovieScreening,
+    @ManyToMany
+    val seats: Set<Seat>,
+    val expirationDate: Date,
+    @Id @GeneratedValue
+    val id: Long = 0,
+)
+
+@Entity
+class Seat(
+    @ManyToOne(fetch = FetchType.LAZY)
+    val screeningRoom: ScreeningRoom,
+    val name: String,
+    @Id @GeneratedValue
+    val id: Long = 0,
+){
+    @ManyToOne(fetch = FetchType.LAZY)
+    var seatToLeft: Seat? = null
+    @ManyToOne(fetch = FetchType.LAZY)
+    var seatToRight: Seat? = null
+}
