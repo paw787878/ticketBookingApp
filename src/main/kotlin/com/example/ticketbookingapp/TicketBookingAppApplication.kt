0fe68130d6bd635c39c1adb5137c2cd5098346bc
Pawel Czajka <paw787878@gmail.com>
2023-05-03 10:56:38 +0200

    initial commit after spring initilizer

---
+++ src/main/kotlin/com/example/ticketbookingapp/TicketBookingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ticketbookingapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketBookingAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketBookingAppApplication>(*args)
+}
