cacf9bb47f067ceda4adfdcf189019c2b82e8747
Pawel Czajka <paw787878@gmail.com>
2023-05-06 19:13:23 +0200

    added simple validation rules

--- src/main/kotlin/com/example/ticketbookingapp/domain/domain.kt
+++ src/main/kotlin/com/example/ticketbookingapp/domain/domain.kt
@@ -1,10 +1,11 @@
 ﻿package com.example.ticketbookingapp.domain
 
 import jakarta.persistence.*
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Size
 import org.springframework.data.jpa.repository.JpaRepository
 import java.math.BigDecimal
 import java.time.Instant
-
 
 // TODO_PAWEL not null and such
 
@@ -12,14 +13,24 @@ import java.time.Instant
 
 @Embeddable
 class User(
+    @Column(nullable = false)
+    @get:Size(min = 3, max = 255)
     var name: String,
+
+    @Column(nullable = false)
+    @get:Size(min = 3, max = 255)
     var surname: String,
 )
 
 @Entity
 class Movie(
+    @Column(nullable = false)
+    @get:Size(max = 255)
     val title: String,
+
+    @Column(nullable = false)
     val lengthInMinutes: Int,
+
     @Id @GeneratedValue
     val id: Long = 0,
 )
@@ -28,7 +39,10 @@ interface MovieRepository : JpaRepository<Movie, Long>
 
 @Entity
 class ScreeningRoom(
+    @Column(nullable = false)
+    @get:Size(max = 255)
     val name: String,
+
     @Id @GeneratedValue
     val id: Long = 0,
 ){
@@ -42,12 +56,18 @@ interface ScreeningRoomRepository : JpaRepository<ScreeningRoom, Long>
 // TODO_PAWEL czy nadpisywac equals?
 @Entity
 class MovieScreening(
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     val movie: Movie,
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     val screeningRoom: ScreeningRoom,
+
+    @Column(nullable = false)
     val timeOfStart: Instant,
+
+    @Column(nullable = false)
     val timeOfEnd: Instant,
+
     @Id @GeneratedValue
     val id: Long = 0,
     // TODO_PAWEL this does not modify in database, investigate
@@ -60,11 +80,16 @@ interface MovieScreeningRepository : JpaRepository<MovieScreening, Long>
 
 @Entity
 class Reservation(
+    @get:Valid
     val user: User,
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     // TODO_PAWEL why not used?
     val movieScreening: MovieScreening,
+
+    @Column(nullable = false)
     val expirationDate: Instant,
+
     @Id @GeneratedValue
     val id: Long = 0,
 )
@@ -73,12 +98,15 @@ interface ReservationRepository : JpaRepository<Reservation, Long>
 
 @Entity
 class ReservationSeat(
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     val reservation: Reservation,
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     val seat: Seat,
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     val ticketType: TicketType,
+
     @Id @GeneratedValue
     val id: Long = 0,
 )
@@ -87,8 +115,13 @@ interface ReservationSeatRepository : JpaRepository<ReservationSeat, Long>
 
 @Entity
 class TicketType(
+    @Column(nullable = false)
+    @get:Size(max = 255)
     val name: String,
+
+    @Column(nullable = false)
     val price: BigDecimal,
+
     @Id @GeneratedValue
     val id: Long = 0,
 )
@@ -97,16 +130,23 @@ interface TicketTypeRepository : JpaRepository<TicketType, Long>
 
 @Entity
 class Seat(
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     val screeningRoom: ScreeningRoom,
+
+    @Column(nullable = false)
+    @get:Size(max = 255)
     val rowName: String,
+
+    @Column(nullable = false)
+    @get:Size(max = 255)
     val columnName: String,
+
     @Id @GeneratedValue
     val id: Long = 0,
 ){
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
     var seatToLeft: Seat? = null
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
     var seatToRight: Seat? = null
 
     init {
