f2e95994649db9e43a964212e6e765f538294f50
Pawel Czajka <paw787878@gmail.com>
2023-05-07 15:26:59 +0200

    added exception message for hibernate validator

--- src/main/kotlin/com/example/ticketbookingapp/Exceptions/ExceptionsHandler.kt
+++ src/main/kotlin/com/example/ticketbookingapp/Exceptions/ExceptionsHandler.kt
@@ -1,6 +1,9 @@
 ﻿package com.example.ticketbookingapp.Exceptions
 
+import jakarta.validation.ConstraintViolationException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.transaction.TransactionSystemException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
@@ -13,5 +16,18 @@ class CustomGlobalExceptionHandler : ResponseEntityExceptionHandler() {
     fun customHandleNotFound(ex : ClientResponseException, request: WebRequest): ResponseEntity<String> {
 
         return ResponseEntity(ex.message, ex.responseStatus);
+    }
+
+    @ExceptionHandler(TransactionSystemException::class)
+    fun customHandleNotFound(ex : TransactionSystemException, request: WebRequest): ResponseEntity<String> {
+        (ex.cause?.cause as? ConstraintViolationException)?.let { constraintViolationException ->
+            val message = constraintViolationException.constraintViolations.joinToString(separator = "\n") { violation ->
+                "${violation.invalidValue.toString()} should satisfy ${violation.message}"
+            }
+            return ResponseEntity(message, HttpStatus.BAD_REQUEST);
+        }
+        // TODO_PAWEL also other probably like optimistic
+
+        return ResponseEntity("Database transaction exception", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
