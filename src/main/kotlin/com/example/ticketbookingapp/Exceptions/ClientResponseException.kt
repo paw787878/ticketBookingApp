763992a4febdb9eb63b182b60f1005f41557cbfc
Pawel Czajka <paw787878@gmail.com>
2023-05-07 14:05:47 +0200

    added custom error handling for rest endpoints

---
+++ src/main/kotlin/com/example/ticketbookingapp/Exceptions/ClientResponseException.kt
@@ -0,0 +1,11 @@
+﻿package com.example.ticketbookingapp.Exceptions
+
+import org.springframework.http.HttpStatus
+
+abstract class ClientResponseException() : RuntimeException() {
+    abstract val responseStatus: HttpStatus
+    abstract fun messageImplementation(): String
+
+    override val message: String
+        get() = messageImplementation()
+}
---
+++ src/main/kotlin/com/example/ticketbookingapp/Exceptions/ExceptionsHandler.kt
@@ -0,0 +1,17 @@
+﻿package com.example.ticketbookingapp.Exceptions
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class CustomGlobalExceptionHandler : ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(ClientResponseException::class)
+    fun customHandleNotFound(ex : ClientResponseException, request: WebRequest): ResponseEntity<String> {
+
+        return ResponseEntity(ex.message, ex.responseStatus);
+    }
+}
