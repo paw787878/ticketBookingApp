002237f91b140c98612c822d382bd605984bb0d1
Pawel <paw787878@gmail.com>
2023-05-10 22:35:39 +0200

    old comment removal

--- src/main/kotlin/com/example/ticketbookingapp/webApi/screenings/dto/ScreeningSelectionRequestDto.kt
+++ src/main/kotlin/com/example/ticketbookingapp/webApi/screenings/dto/ScreeningSelectionRequestDto.kt
@@ -3,7 +3,6 @@
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.Instant
 
-// TODO_PAWEL can they be like private?
 data class ScreeningSelectionRequestDto(
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     val minimalStartTime: Instant,
